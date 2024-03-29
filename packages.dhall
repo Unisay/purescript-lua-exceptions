d4422e716d38f0e579188c74e8a75af91d039b1a
Yura Lazarev <Unisay@users.noreply.github.com>
2024-03-29 21:43:18 +0100

    Wrapped foreigns

--- packages.dhall
+++ packages.dhall
@@ -1,9 +1,9 @@
 let upstream-ps =
-      https://github.com/purescript/package-sets/releases/download/psc-0.15.9-20230715/packages.dhall
-        sha256:ca2801f7422d563de4ea4524efe6fa290186d202067409bc9cf359bb23acdfc5
+      https://github.com/purescript/package-sets/releases/download/psc-0.15.15-20240320/packages.dhall
+        sha256:ae8a25645e81ff979beb397a21e5d272fae7c9ebdb021a96b1b431388c8f3c34
 
 let upstream-lua =
-      https://github.com/Unisay/purescript-lua-package-sets/releases/download/psc-0.15.9-20230706/packages.dhall
-        sha256:de2604dd3797c479420a154955209e008fe2cd3fc8452a0bd4b32a2ca00a2ff6
+      https://github.com/Unisay/purescript-lua-package-sets/releases/download/psc-0.15.15-20240332/packages.dhall
+        sha256:64acc80665474662306cebe99a749097340777795a2d8f9edaac8dd4aaafe86f
 
 in  upstream-ps // upstream-lua
--- src/Effect/Exception.lua
+++ src/Effect/Exception.lua
@@ -1,27 +1,21 @@
 return {
-    showErrorImpl = function(err) return err end,
-    error = function(msg) return msg end,
-    errorWithCause = function(msg)
-        return function(cause) return msg .. "\nCaused by: " .. cause end
-    end,
-    message = function(err) return err end,
-    name = function(err) return err end,
-    stackImpl = function()
-        return function(nothing)
-            return function() return nothing end
+  showErrorImpl = (function(err) return err end),
+  error = (function(msg) return msg end),
+  errorWithCause = (function(msg) return function(cause) return msg .. "\nCaused by: " .. cause end end),
+  message = (function(err) return err end),
+  name = (function(err) return err end),
+  stackImpl = (function() return function(nothing) return function() return nothing end end end),
+  throwException = (function(err) return function() error(err) end end),
+  catchException = (function(c)
+    return function(t)
+      return function()
+        local ok, errorOrResult = pcall(t)
+        if ok then
+          return errorOrResult
+        else
+          return c(errorOrResult)
         end
-    end,
-    throwException = function(err) return function() error(err) end end,
-    catchException = function(c)
-        return function(t)
-            return function()
-                local ok, errorOrResult = pcall(t)
-                if ok then
-                    return errorOrResult
-                else
-                    return c(errorOrResult)
-                end
-            end
-        end
+      end
     end
+  end)
 }
